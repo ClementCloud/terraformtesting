0a98905c32cfce32c07e6228e04890ec3f105a4a
Clement Familusi <clementfamilusi@Clements-MacBook-Pro-2.local>
2023-07-31 23:10:04 -0400

    migratedstate

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 provider "aws" {
-  region     = "us-west-2"
+  region = "us-west-2"
+}
 
+terraform {
+  backend "s3" {
+    bucket = "cjtestbucket072923"
+    key    = "tfstate/tfstatetest1"
+    region = "us-west-2"
+  }
 }
